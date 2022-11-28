ad247acc4f7dd964b0cb7d16c59f6d1205a8095d
Derek DeRoo <derek.l.deroo@gmail.com>
2022-11-28 12:21:36 -0600

    commit tree 55d7fbe91d7e4d8bdc44002d3157e1f3a03aee0f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const messages = []
+
+function writeCards(stringName, eventName) {
+    for(let i = 0; i<stringName.length; i++) {
+
+        messages.push((`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`));
+    }
+
+return messages;
+}
+function countDown(integerPos) {
+    let i = integerPos;
+    while(i >= 0){
+        console.log(i--);
+    }
+}
+
+countDown(10);
